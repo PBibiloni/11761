93dfdca61178c0c0252ce8fab31b2e68934ed959
PBibiloni <pedro@bibiloni.es>
2022-09-27 15:40:51 +0200

    Remove solutions

--- lesson02/task02_convolution.py
+++ lesson02/task02_convolution.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 
 def convolution(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
-    """Returns the convolution of one image with a specific kernel (with no padding)."""
+    """Sequence of limits of each bin (e.g. [0.0, 85.0, 170.0, 255.0] for 3 bins)."""
     # YOUR CODE HERE
     # ...
     img_sz_x, img_sz_y = image.shape
@@ -16,9 +16,6 @@ def convolution(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
         for j in range(out_sz_y):
             # YOUR CODE HERE
             # ...
-            for k in range(krn_sz_x):
-                for l in range(krn_sz_y):
-                    out[i, j] += image[i + (krn_sz_x-1) - k, j + (krn_sz_y-1) - l] * kernel[k, l]
     return out
 
 

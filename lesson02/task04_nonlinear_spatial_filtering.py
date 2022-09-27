dafe014e43145c85c9b30b484b3f74e6c86b3f1e
PBibiloni <pedro@bibiloni.es>
2022-09-27 15:42:19 +0200

    Remove annotations

--- lesson02/task04_nonlinear_spatial_filtering.py
+++ lesson02/task04_nonlinear_spatial_filtering.py
@@ -7,16 +7,12 @@ from matplotlib import pyplot as plt
 
 def median_filter(image: np.ndarray, filter_size: Tuple[int, int]) -> np.ndarray:
     """Returns an image after applying the median filter of the given size."""
-    # YOUR CODE HERE: see `np.median(...)`
-    # ...
     img_sz_x, img_sz_y = image.shape
     out_sz_x = img_sz_x - filter_size[0] + 1  # Why?
     out_sz_y = img_sz_y - filter_size[1] + 1  # Why?
     out = np.zeros(shape=(out_sz_x, out_sz_y), dtype=image.dtype)
     for i in range(out_sz_x):
         for j in range(out_sz_y):
-            # YOUR CODE HERE
-            # ...
             values = image[i:i + (filter_size[0]-1), j:j + (filter_size[1]-1)]
             out[i, j] = np.median(values)
     return out
@@ -24,16 +20,12 @@ def median_filter(image: np.ndarray, filter_size: Tuple[int, int]) -> np.ndarray
 
 def max_pooling(image: np.ndarray, pool_size: Tuple[int, int]) -> np.ndarray:
     """Returns an image after applying the max pooling of the given size."""
-    # YOUR CODE HERE: see `np.max(...)`
-    # ...
     img_sz_x, img_sz_y = image.shape
     out_sz_x = img_sz_x//pool_size[0]   # Why?
     out_sz_y = img_sz_y//pool_size[1]   # Why?
     out = np.zeros(shape=(out_sz_x, out_sz_y), dtype=image.dtype)
     for i in range(out_sz_x):
         for j in range(out_sz_y):
-            # YOUR CODE HERE
-            # ...
             values = image[i*pool_size[0]:(i+1)*pool_size[0], j*pool_size[1]:(j+1)*pool_size[1]]
             out[i, j] = np.max(values)
     return out

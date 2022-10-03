f5eb1a172990a935aa2835bf153b1051375439a0
PBibiloni <pedro@bibiloni.es>
2022-10-03 15:05:54 +0200

    Add solutions

--- lesson03/task02_compute_cc.py
+++ lesson03/task02_compute_cc.py
@@ -1,5 +1,3 @@
-import os
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -55,24 +53,10 @@ def label_connected_components(binary_img: np.ndarray) -> np.ndarray:
     return label_img
 
 
-def binarize_by_hysteresis(img: np.ndarray, low_threshold: float, high_threshold: float) -> np.ndarray:
-    """Returns a binary version of the image by applying a hysteresis operation."""
-    out = np.zeros_like(img)
-    label_img = label_connected_components(binarize_by_thresholding(img, low_threshold))
-    labels = np.unique(label_img)
-    for label in labels:
-        # YOUR CODE HERE: see `np.any(...)` and `np.all(...)`.
-        # ...
-        if np.any(img[label_img == label] >= high_threshold):
-            out[label_img == label] = 255
-    return out
-
-
 if __name__ == "__main__":
     # Show effect
     img = cv2.imread('../samples/airplane.tiff', cv2.IMREAD_GRAYSCALE)  # Read the image.
     img = img.astype('float32')  # Convert to float32 to avoid overflow and rounding errors
-    # img = cv2.resize(img, dsize=(img.shape[1]//20, img.shape[0]//20))
 
     # Visualize CCs for different thresholds
     fig, axs = plt.subplots(2, 2)
@@ -86,27 +70,4 @@ if __name__ == "__main__":
             cmap='Pastel1')
     # Display figure
     plt.show()
-
-    # Visualize Hystersis for different upper thresholds
-    fig, axs = plt.subplots(2, 2)
-    # Remove default axis
-    for ax in axs.flatten():
-        ax.axis('off')
-    # Show one image per subplot
-    axs.flatten()[0].set_title('Orginal')
-    axs.flatten()[0].imshow(img, cmap='gray')
-    for ax, upper_th in zip(axs.flatten()[1:], [192, 208, 224]):
-        ax.set_title(f'Hysteresis (192, {upper_th:d})')
-        ax.imshow(
-            binarize_by_hysteresis(img, low_threshold=128, high_threshold=upper_th),
-            cmap='gray')
-    # Display figure
-    plt.show()
-
-
-    # Save images
-    os.makedirs('../results', exist_ok=True)
-    out = binarize_by_hysteresis(img, low_threshold=128, high_threshold=192)
-    cv2.imwrite(f'../results/task02_hyst_128_192.png', out.astype('uint8'))
-    cv2.imwrite(f'../results/task02_hyst_128_192_detail.png', out[175:225, 70:120].astype('uint8'))
 
--- lesson03/task04_mathematical_morphology.py
+++ lesson03/task04_mathematical_morphology.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+import lesson03.task01_binarize as task01
 
 
 def dilation(binary_img: np.ndarray, structuring_element: np.ndarray) -> np.ndarray:
@@ -30,20 +33,57 @@ def closing(binary_img: np.ndarray, structuring_element: np.ndarray) -> np.ndarr
     return erosion(dilation(binary_img, structuring_element), structuring_element)
 
 
-def morphological_gradient(binary_img: np.ndarray) -> np.ndarray:
+def morphological_gradient(binary_img: np.ndarray, structuring_element: np.ndarray) -> np.ndarray:
     """Returns the morphological gradient of the binary image with the given structuring element."""
-    structuring_element = np.ones((3, 3), dtype='uint8')    # 8-connectivity.
     # YOUR CODE HERE: see `cv2.morphologyEx`
     # ...
-    # TODO
-    return cv2.morphologyEx(binary_img.astype('uint8'), cv2.MORPH_GRADIENT, structuring_element.astype('uint8'))
+    return dilation(binary_img, structuring_element) - erosion(binary_img, structuring_element)
 
 
-def morphological_skeleton(binary_img: np.ndarray) -> np.ndarray:
-    """Returns the morphological skeleton of the binary image considering the Lantuéjoul's formula."""
-    structuring_element = np.ones((3, 3), dtype='uint8')    # 8-connectivity.
+def morphological_skeleton(binary_img: np.ndarray, structuring_element: np.ndarray) -> np.ndarray:
+    """Returns the morphological skeleton of the binary image considering Lantuéjoul's method."""
     # YOUR CODE HERE: see `cv2.morphologyEx`
     # ...
-    # TODO
-    return cv2.morphologyEx(binary_img.astype('uint8'), cv2.MORPH_SKELETON, structuring_element.astype('uint8'))
+    # Iteratively erode the image (until there are no more pixels)
+    eroded_imgs = []
+    current_img = binary_img
+    while np.any(current_img):
+        eroded_imgs.append(current_img)
+        current_img = erosion(current_img, structuring_element)
 
+    skeleton = np.zeros_like(binary_img)
+    for eroded_img in eroded_imgs:
+        skeleton += eroded_img - opening(eroded_img, structuring_element)
+    return skeleton
+
+
+if __name__ == "__main__":
+    # Visualize morphological operations on binary and grayscale images
+    binary_img = task01.binarize_by_Otsu(cv2.imread("../samples/dots.tiff", cv2.IMREAD_GRAYSCALE))
+    grayscale_img = cv2.imread("../samples/mandril.tiff", cv2.IMREAD_GRAYSCALE)
+
+    structuring_element = np.ones((3, 3), dtype='uint8')    # 8-connectivity.
+
+    for img in [binary_img, grayscale_img]:
+        results = {
+            'erosion': erosion(img, structuring_element),
+            'dilation': dilation(img, structuring_element),
+            'original': img,
+            'opening': opening(img, structuring_element),
+            'closing': closing(img, structuring_element),
+            'gradient': morphological_gradient(img, structuring_element),
+            'skeleton': morphological_skeleton(img, structuring_element),
+        }
+        results = {k: v for k, v in results.items() if v is not None}  # Remove None values.
+
+        # Visualize images
+        fig, axs = plt.subplots(3, 3)
+        # Remove default axis
+        for ax in axs.flatten():
+            ax.axis('off')
+        # Show one image per subplot
+        for ax, (title, img) in zip(axs.flatten(), results.items()):
+            ax.set_title(title)
+            ax.imshow(img, cmap='gray')
+        # Display figure
+        plt.show()

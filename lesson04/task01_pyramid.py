29fd386f784b1579580ef7641dff3eea7af277e5
PBibiloni <pedro@bibiloni.es>
2022-10-12 11:46:30 +0200

    Add applications of Laplacian pyramid

--- lesson04/task01_pyramid.py
+++ lesson04/task01_pyramid.py
@@ -17,23 +17,55 @@ def laplacian_pyramid(img: np.ndarray, levels: int) -> tp.List[np.ndarray]:
     # ...
 
 
+def reconstruct_from_laplacian_pyramid(l_pyramid: tp.List[np.ndarray]) -> np.ndarray:
+    """Reconstructs an image from its Laplacian pyramid."""
+    # Your code here: see `cv.pyrUp(...)`, and start from the smallest layer.
+    # ...
+
+
+def remove_finer_detail(img: np.ndarray) -> np.ndarray:
+    """Removes the finer details of the image by applying a Laplacian pyramid."""
+    original_img = cv2.imread('../samples/airplane.tiff', cv2.IMREAD_GRAYSCALE).astype('float32')
+    l_pyramid = laplacian_pyramid(original_img, levels=6)
+    # Your code here: how many `levels` will you remove? how?
+    # ...
+
+
 if __name__ == "__main__":
     # Show effect
     original_img = cv2.imread('../samples/airplane.tiff', cv2.IMREAD_GRAYSCALE)  # Read the image.
     original_img = original_img.astype('float32')  # Convert to float32 to avoid overflow and rounding errors
 
-    results = {
+    pyramids = {
         'Gaussian': gaussian_pyramid(original_img, 4),
         'Laplacian': laplacian_pyramid(original_img, 4),
     }
-    results = {k: v for k, v in results.items() if v is not None}  # Remove None values.
+    pyramids = {k: v for k, v in pyramids.items() if v is not None}  # Remove None values.
 
-    for method_name, pyramid in results.items():
+    for method_name, pyramid in pyramids.items():
         # Visualize images
         fig, axs = plt.subplots(1, 4)
         fig.suptitle(method_name)
         # Show one image per subplot
         for ax, subimage in zip(axs.flatten(), pyramid):
+            ax.imshow(subimage, cmap='gray')
+        # Display figure
+        plt.show()
+
+    # Show effect of removing coarse details
+    if 'Laplacian' in pyramids:
+        l_pyramid = pyramids['Laplacian']
+        reconstructions = {
+            'Original': original_img,
+            'Recovered': reconstruct_from_laplacian_pyramid(l_pyramid),
+            'Without coarse detail': remove_finer_detail(original_img),
+        }
+        reconstructions = {k: v for k, v in reconstructions.items() if v is not None}  # Remove None values.
+        # Visualize images
+        fig, axs = plt.subplots(1, 3)
+        # Show one image per subplot
+        for ax, (name, subimage) in zip(axs.flatten(), reconstructions.items()):
+            ax.set_title(name)
             ax.imshow(subimage, cmap='gray')
         # Display figure
         plt.show()

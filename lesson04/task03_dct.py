1605b501c89cae2d14a048d263efdb6e78cd5cf7
PBibiloni <pedro@bibiloni.es>
2022-10-12 17:56:35 +0200

    Improve visualization

--- lesson04/task03_dct.py
+++ lesson04/task03_dct.py
@@ -28,37 +28,30 @@ def center_coefficients(dct_coefficients: np.ndarray) -> np.ndarray:
 
 
 if __name__ == "__main__":
-    # Show effect
     original_img = cv2.imread('../samples/mandril.tiff', cv2.IMREAD_GRAYSCALE).astype('float32') / 255.0
+    # Direct and invert DCT transform
     dct_coefficients = discrete_cosinus_transform(original_img)
     shifted_dct_coefficients = center_coefficients(dct_coefficients)
-    removed_dct_coefficients = remove_last_coefficients(shifted_dct_coefficients, 30, 30)
-
     recovered_img = invert_discrete_consinus_transform(dct_coefficients)
+    # Remove some coefficients and invert DCT transform
+    removed_dct_coefficients = remove_last_coefficients(dct_coefficients, 30, 30)
+    shifted_and_removed_dct_coefficients = center_coefficients(removed_dct_coefficients)
     filtered_img = invert_discrete_consinus_transform(removed_dct_coefficients)
 
-    results = {
-        'Original': original_img,
-        'DCT coefficients': dct_coefficients,
-        'DCT coefficients (cent)': shifted_dct_coefficients,
-        'Recovered': recovered_img,
-    }
+    # Visualize the effect of computing and reverting the DCT
     fig, axs = plt.subplots(1, 4)
-    # Show one image per subplot
-    for ax, (name, subimage) in zip(axs.flatten(), results.items()):
-        ax.imshow(subimage, cmap='gray')
-    # Display figure
+    fig.suptitle('Forward and inverse DCT')
+    axs[0].imshow(original_img, cmap='gray')
+    axs[1].imshow(np.abs(dct_coefficients), cmap='gray', norm=colors.PowerNorm(gamma=0.05))
+    axs[2].imshow(np.abs(shifted_dct_coefficients), cmap='gray', norm=colors.PowerNorm(gamma=0.05))
+    axs[3].imshow(recovered_img, cmap='gray')
     plt.show()
 
-    results = {
-        'Original': original_img,
-        'DCT coefficients (cent)': shifted_dct_coefficients,
-        'DCT coefficients (cent, 30x30 removed)': removed_dct_coefficients,
-        'Recovered': filtered_img,
-    }
+    # Visualize the effect of removing the last coefficients
     fig, axs = plt.subplots(1, 4)
-    # Show one image per subplot
-    for ax, (name, subimage) in zip(axs.flatten(), results.items()):
-        ax.imshow(subimage, cmap='gray')
-    # Display figure
+    fig.suptitle('Removing last DCT coefficients')
+    axs[0].imshow(original_img, cmap='gray')
+    axs[1].imshow(np.abs(shifted_dct_coefficients), cmap='gray', norm=colors.PowerNorm(gamma=0.05))
+    axs[2].imshow(np.abs(shifted_and_removed_dct_coefficients), cmap='gray', norm=colors.PowerNorm(gamma=0.05))
+    axs[3].imshow(filtered_img, cmap='gray')
     plt.show()

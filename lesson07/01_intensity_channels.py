fc195fafc90a2dca434a5d6502d1cc96674e8234
PBibiloni <pedro@bibiloni.es>
2022-11-11 17:24:21 +0100

    Add solutions

--- lesson07/01_intensity_channels.py
+++ lesson07/01_intensity_channels.py
@@ -5,39 +5,41 @@ from matplotlib import pyplot as plt
 
 def intensity(img_bgr: np.ndarray) -> np.ndarray:
     """ Compute the intensity of an image. """
-    # YOUR CODE HERE
-    # ...
+    img_bgr = img_bgr.astype('float')
+    img_gray = (img_bgr[:, :, 0] + img_bgr[:, :, 1] + img_bgr[:, :, 2]) / 3
+    return img_gray.astype('uint8')
 
 
 def luma(img_bgr: np.ndarray) -> np.ndarray:
     """ Compute the intensity of an image. """
-    # YOUR CODE HERE
-    # ...
+    img_bgr = img_bgr.astype('float')
+    img_gray = 0.2126*img_bgr[:, :, 2] + 0.7152*img_bgr[:, :, 1] + 0.0722*img_bgr[:, :, 0]
+    return img_gray.astype('uint8')
 
 
 def value(img_bgr: np.ndarray) -> np.ndarray:
     """ Compute the intensity of an image. """
-    # YOUR CODE HERE
-    # ...
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+    return img_hsv[:, :, 2]
 
 
 def lightness_from_hsl(img_bgr: np.ndarray) -> np.ndarray:
     """ Compute the intensity of an image. """
-    # YOUR CODE HERE
-    # ...
+    img_hls = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HLS)
+    return img_hls[:, :, 1]
 
 
 def lightness_from_cielab(img_bgr: np.ndarray) -> np.ndarray:
     """ Compute the intensity of an image. """
-    # YOUR CODE HERE
-    # ...
+    img_hsl = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2Lab)
+    return img_hsl[:, :, 0]
 
 
 if __name__ == "__main__":
     img_bgr = cv2.imread('../samples/peppers.tiff', cv2.IMREAD_COLOR)
 
     results = {
-        # 'original': img_bgr,
+        'original': img_bgr,
         'intensity': intensity(img_bgr),
         'luma': luma(img_bgr),
         'value': value(img_bgr),
@@ -47,7 +49,7 @@ if __name__ == "__main__":
     results = {k: v for k, v in results.items() if v is not None}
 
     # Visualize images
-    fig, axs = plt.subplots(1, len(results))
+    fig, axs = plt.subplots(2, 3)
     [ax.axis('off') for ax in axs.flatten()]
     # Show one image per subplot
     for ax, (title, subimage) in zip(axs.flatten(), results.items()):
--- lesson07/02_multichannel_edge_detection.py
+++ lesson07/02_multichannel_edge_detection.py
@@ -3,33 +3,31 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def process_on_best_channel(img: np.ndarray) -> np.ndarray:
+def process_on_best_channel(img_bgr: np.ndarray) -> np.ndarray:
     """ Apply a Canny edge detector on the `best` channel. """
-    # YOUR CODE HERE: how do you define the best channel?
-    # ...
-    best_channel = img[:, :, 0]
-    # or best_channel = img[:, :, 1]
-    # or best_channel = img[:, :, 2]
+    best_channel = img_bgr[:, :, 1]  # Green channel
+    return cv2.Canny(best_channel, 100, 200)
 
-    # YOUR CODE HERE: see cv2.Canny(...)
-    # ...
 
-def process_on_intensity_channel(img: np.ndarray) -> np.ndarray:
+def process_on_intensity_channel(img_bgr: np.ndarray) -> np.ndarray:
     """ Apply a Canny edge detector on the intensity channel. """
-    # YOUR CODE HERE: what is the best `intensity` channel?
-    # ...
+    img_lab = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2LAB)
+    return cv2.Canny(img_lab[:, :, 0], 100, 200)
 
-def parallel_channels_then_combine(img: np.ndarray) -> np.ndarray:
+
+def parallel_channels_then_combine(img_bgr: np.ndarray) -> np.ndarray:
     """ Apply a Canny edge detector on each channel, then combine them. """
-    # YOUR CODE HERE: see cv2.bitwise_or(...), cv2.bitwise_and(...)
-    # ...
+    all_edges = np.zeros(shape=img_bgr.shape[:2], dtype=img_bgr.dtype)
+    for band in range(img_bgr.shape[-1]):
+        all_edges = np.bitwise_or(all_edges, cv2.Canny(img_bgr[:, :, band], 100, 200))
+    return all_edges
 
 
 if __name__ == "__main__":
     img_bgr = cv2.imread('../samples/peppers.tiff', cv2.IMREAD_COLOR)
 
     results = {
-        # 'original': img_bgr,
+        'original': img_bgr,
         'Best channel': process_on_best_channel(img_bgr),
         'Intensity': process_on_intensity_channel(img_bgr),
         'In parallel then combine': parallel_channels_then_combine(img_bgr),
@@ -37,7 +35,7 @@ if __name__ == "__main__":
     results = {k: v for k, v in results.items() if v is not None}
 
     # Visualize images
-    fig, axs = plt.subplots(1, len(results))
+    fig, axs = plt.subplots(2, 2)
     [ax.axis('off') for ax in axs.flatten()]
     # Show one image per subplot
     for ax, (title, subimage) in zip(axs.flatten(), results.items()):
